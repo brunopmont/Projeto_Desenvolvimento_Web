fea0807b0dca10ea9c415e0d2ed21aa5716dac4a
Bruno Porto <bpmonteiro@id.uff.br>
2025-12-06 15:17:12 -0300

    Adição de controllers

--- restapi/auth/Controller/UsuarioController.java
+++ restapi/auth/Controller/UsuarioController.java
@@ -1,4 +1,23 @@
-package com.bruno.restapi.controller;
+package com.bruno.restapi.auth.controller;
 
+import com.bruno.restapi.auth.model.Usuario;
+import com.bruno.restapi.auth.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
+@RequiredArgsConstructor
 public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
+        return ResponseEntity.ok(usuarioService.salvar(usuario));
+    }
 }
